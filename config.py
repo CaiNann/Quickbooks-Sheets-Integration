ab22758e66c3c78f0a990bcc10b755717cebd1a9
Cainan Enneking <cainanenneking2@gmail.com>
2024-06-23 18:14:44 -0700

    changed redirect_uri to new static domain

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 client_id = 'ABELy2kJPVjKIaWycZKD8B9rF1fPLpMyxq5Y5zhgOY8jd5l4Fj'
 client_secret = '05EYhumz1oXvKQqiCOVsVYhCvUZ2OV4Pi688esWL'
-redirect_uri = 'https://2b03-76-170-113-56.ngrok-free.app/callback'
+redirect_uri = 'https://duckling-arriving-highly.ngrok-free.app/callback'
 environment = 'sandbox'
 realm_id = '9341452557106265'
 token_endpoint = 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer'
